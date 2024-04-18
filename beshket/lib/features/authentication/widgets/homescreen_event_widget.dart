7cc63a1ebf8bc1f695d868a39fa24e475c6fc064
martinpodmanicky <mpodmanicky34@gmail.com>
2024-04-18 21:21:13 +0200

    idem/windows

---
+++ beshket/lib/features/authentication/widgets/homescreen_event_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventWidget extends StatefulWidget {
+  const EventWidget({super.key});
+
+  @override
+  State<EventWidget> createState() => _EventWidgetState();
+}
+
+class _EventWidgetState extends State<EventWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
