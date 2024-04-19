ec2eb2f119e7373294868fdb8fb1b46ae575a281
martinpodmanicky <mpodmanicky34@gmail.com>
2024-04-19 11:07:03 +0200

    revert-bloc-homescreen

--- beshket/lib/features/authentication/screens/home_screen.dart
+++ beshket/lib/features/authentication/screens/home_screen.dart
@@ -1,13 +1,11 @@
 //Author: <Martin Podmanicky>
 
-import 'package:beshket/blocs/bottom_nav_cubit.dart';
 import 'package:beshket/features/authentication/screens/cart_screen.dart';
 import 'package:beshket/features/authentication/screens/chat_screen.dart';
 import 'package:beshket/features/authentication/screens/homescreen_after_auth.dart';
 import 'package:beshket/features/authentication/screens/tickets_screen.dart';
 import 'package:beshket/features/authentication/widgets/destinations.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../services/auth_firebase.dart';
 
@@ -57,39 +55,39 @@ class _HomePageState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: BlocProvider(
-        create: (context) => BottomNavCubit(),
-        child: SafeArea(
-          child: PageView(
-            controller: PageController(initialPage: currentPageIndex),
-            onPageChanged: (index) =>
-                context.read<BottomNavCubit>().changeSelectedIndex(index),
-            children: <Widget>[
-              MainScreen(),
-              Tickets(),
-              Chats(),
-              Cart(),
-            ],
-          ),
+      body: SafeArea(
+        child: PageView(
+          controller: PageController(initialPage: currentPageIndex),
+          onPageChanged: (index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          children: <Widget>[
+            MainScreen(),
+            Tickets(),
+            Chats(),
+            Cart(),
+          ],
         ),
       ),
       //navigation bar reusable
-      bottomNavigationBar: BlocProvider.value(
-        value: BlocProvider.of<BottomNavCubit>(context),
-        child: NavigationBar(
-          elevation: 1,
-          backgroundColor: Colors.white,
-          indicatorColor: const Color.fromARGB(255, 213, 233, 241),
-          destinations: destinations.map<NavigationDestination>((d) {
-            return NavigationDestination(
-              icon: Icon(d.icon),
-              label: d.label,
-            );
-          }).toList(),
-          selectedIndex: BlocProvider.of<BottomNavCubit>(context).state,
-          onDestinationSelected: (index) =>
-              context.read<BottomNavCubit>().changeSelectedIndex(index),
-        ),
+      bottomNavigationBar: NavigationBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        indicatorColor: const Color.fromARGB(255, 213, 233, 241),
+        destinations: destinations.map<NavigationDestination>((d) {
+          return NavigationDestination(
+            icon: Icon(d.icon),
+            label: d.label,
+          );
+        }).toList(),
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
       ),
     );
   }
